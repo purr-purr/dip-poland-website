f6b737b2082c85c70196e5fb7258736bfcb5cce2
Tony <56505654+666hesoyam@users.noreply.github.com>
2021-06-16 19:56:36 +0300

    upd

--- HTML/js/components/popup.js
+++ HTML/js/components/popup.js
@@ -5,3 +5,24 @@ $('#popup-default').popup({
     closebutton: true,
     escape: false
 });
+
+
+
+// Способ 1
+
+// В коде который отвечает за отправку формы/запись данных, вставить строку которая будет вызывать функцию открытия поп-апа.
+
+// эта строка вызывает её
+// openPopup(); 
+
+// сама функция
+// function openPopup() {
+//     $('#popup-default').popup('show');
+// }
+
+
+// Способ 2
+// Тоже самое что способ 1, только проще в реализации.
+// $('.subscription__form').on('submit', function(e) {
+//     $('#popup-default').popup('show');
+// });
